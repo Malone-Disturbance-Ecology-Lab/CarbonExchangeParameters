99703b78ee0b0261b64f068a83348d4b481b83bb
jnawang <wangjunna2013@gmail.com>
2025-02-25 15:21:04 -0500

    change Pm to Pmax

--- LRC_PARMS_06.R
+++ LRC_PARMS_06.R
@@ -23,17 +23,17 @@ message("Using the LRC_PARMS function requires the following libraries: brms,
 
 message("This function uses the equation:
         
-        NEE ~ Pm * (1 - exp(alpha * (PAR - Icomp)))
+        NEE ~ Pmax * (1 - exp(alpha * (PAR - Icomp)))
         
         Where nee is daytime net ecosystem exchange (𝜇mol CO2 m-2 s-1), 
-        Pm: the maximum photosynthesis rate (𝜇mol CO2 m-2 s-1), 
+        Pmax: the maximum photosynthesis rate (𝜇mol CO2 m-2 s-1), 
         alpha is a coefficient,
         Icomp is the compensation light level with the same unit of PAR.
         
         The equations require PAR (𝜇mol m-2 s-1) and NEE (𝜇mol m-2 s-1)")
 
 
-priors.lrc <-  prior(normal(-5, 5), nlpar = "Pm", lb=-30, ub= 0) +
+priors.lrc <-  prior(normal(-5, 5), nlpar = "Pmax", lb=-30, ub= 0) +
   prior(normal(0.1, 1), nlpar = "alpha", lb=0, ub= 1) +
   prior(normal(300, 100), nlpar = "Icomp", lb=0, ub= 500)
 
@@ -50,7 +50,7 @@ LRC_PARMS_06 <- function( data.frame, iterations, priors.lrc, idx.colname, NEE.c
                               nee,
                               PAR)
   
-  equation <- nee ~ Pm * (1 - exp(alpha * (PAR - Icomp)))
+  equation <- nee ~ Pmax * (1 - exp(alpha * (PAR - Icomp)))
   
   
   if( c("idx") %in% names(df) ) {
@@ -67,11 +67,11 @@ LRC_PARMS_06 <- function( data.frame, iterations, priors.lrc, idx.colname, NEE.c
   
   # PARM Dataframe:
   parms <- data.frame(idx=as.character(), 
-                      Pm.mean = as.numeric(), 
-                      Pm.se = as.numeric(),
-                      Pm.Bulk_ESS= as.numeric() ,
-                      Pm.Tail_ESS= as.numeric() ,
-                      Pm.Rhat= as.numeric(),
+                      Pmax.mean = as.numeric(), 
+                      Pmax.se = as.numeric(),
+                      Pmax.Bulk_ESS= as.numeric() ,
+                      Pmax.Tail_ESS= as.numeric() ,
+                      Pmax.Rhat= as.numeric(),
                       
                       alpha.mean = as.numeric(),
                       alpha.se = as.numeric(),
@@ -97,13 +97,13 @@ LRC_PARMS_06 <- function( data.frame, iterations, priors.lrc, idx.colname, NEE.c
     # get priors:
     #priors <- get_prior(bf(equation, a1+ax+r ~ 1, nl=TRUE),data = df %>% filter(PAR > 0), family = poisson())
     
-    try(model.brms <- brm( bf( equation, Pm+alpha+Icomp ~ 1, nl=TRUE), prior = priors.lrc, 
+    try(model.brms <- brm( bf( equation, Pmax+alpha+Icomp ~ 1, nl=TRUE), prior = priors.lrc, 
                            data = df.sub, iter = iterations, cores =3, chains = 1, backend = "cmdstanr"), silent= F)
     # Here it should be df.sub
     print(model.brms)
     
     try(model.brms.df <- summary(model.brms)$fixed , silent = T)
-    try(model.brms.df.Pm <- model.brms.df %>% filter( row.names(model.brms.df) == 'Pm_Intercept'), silent = F)
+    try(model.brms.df.Pmax <- model.brms.df %>% filter( row.names(model.brms.df) == 'Pmax_Intercept'), silent = F)
     try(model.brms.df.alpha <- model.brms.df %>% filter( row.names(model.brms.df) == 'alpha_Intercept'), silent = F)
     try(model.brms.df.Icomp <- model.brms.df %>% filter( row.names(model.brms.df) == 'Icomp_Intercept'), silent = F)
     
@@ -112,11 +112,11 @@ LRC_PARMS_06 <- function( data.frame, iterations, priors.lrc, idx.colname, NEE.c
     baseline <- as.Date(paste(i, '-01', sep="")) %>% lubridate::days_in_month() *48 %>% as.numeric
     
     try(results <- data.frame( idx = i, 
-                               Pm.mean = model.brms.df.Pm$Estimate ,
-                               Pm.se = model.brms.df.Pm$Est.Error ,
-                               Pm.Bulk_ESS = model.brms.df.Pm$Bulk_ESS , 
-                               Pm.Tail_ESS = model.brms.df.Pm$Tail_ESS ,
-                               Pm.Rhat = model.brms.df.Pm$Rhat ,
+                               Pmax.mean = model.brms.df.Pmax$Estimate ,
+                               Pmax.se = model.brms.df.Pmax$Est.Error ,
+                               Pmax.Bulk_ESS = model.brms.df.Pmax$Bulk_ESS , 
+                               Pmax.Tail_ESS = model.brms.df.Pmax$Tail_ESS ,
+                               Pmax.Rhat = model.brms.df.Pmax$Rhat ,
                                
                                alpha.mean = model.brms.df.alpha$Estimate ,
                                alpha.se = model.brms.df.alpha$Est.Error ,
@@ -170,145 +170,3 @@ message("Model parameters Are fit using the R package brms.
 
 
 # EOF
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#' #' fit a Light Response Curves (LRC) by an index to get a parameter file
-#' #'
-#' #' `LRC_PARMS` returns a dataframe of parameter values by the index used to fit them.
-#' #' `LRC_PARMS` requires a few arguments @data.frame,  @iterations, and @priors.
-#' #' `dataframe` is a dataframe that containes nee, an index, and PAR.
-#' #' `iterations` is the number of iterations to run brms. 
-#' #' `priors.LRC ` is the priors for the brms to use.
-#' #' `idx` is the column containing the index.
-#' #' `nee` is the name of the column containing NEE.
-#' #' `PAR` is the name of the column containing PAR.
-#' #' `TA` is the name of the column containing air temperature.
-#' 
-#' 
-#' # Add notes to indicate successful or unsuccessful fit. 
-#' 
-#' library(brms) 
-#' library(cmdstanr)
-#' library(ggplot2)
-#' library(beepr)
-#' library(tidybayes)
-#' library(tidyverse)
-#' library(ggpubr)
-#' 
-#' 
-#' # Example of priors: 
-#' priors.lrc <-  prior(normal(-5, 5), nlpar = "Pm", lb=-30, ub= 0) +
-#'   prior(normal(0.1, 1), nlpar = "alpha", lb=0, ub= 1) +
-#'   prior(normal(100, 100), nlpar = "Icomp", lb=0, ub= 500)
-#' 
-#' LRC_PARMS_06 <- function( data.frame, iterations, priors.lrc, idx, nee, PAR){
-#'   
-#'   df <- data.frame %>% mutate(idx= idx,
-#'                               nee= nee,
-#'                               PAR= PAR ) %>% select(idx, nee, PAR)
-#'   
-#'   equation <- nee ~ Pm * (1 - exp(alpha * (PAR - Icomp)))
-#'   
-#'   
-#'   if( c("idx") %in% names(df) ) {
-#'     print("idx present")
-#'   } else{ print("The dataframe must include: idx,  nee, and PAR")}
-#'   
-#'   if( c("nee") %in% names(df) ) {
-#'     print("nee data present")
-#'   }  else{ print("The dataframe must include: idx,  nee, and PAR")}
-#'   
-#'   if( c("PAR") %in% names(df) ) {
-#'     print("PAR data present")
-#'   } else{ print("The dataframe must include: idx,  nee, and PAR")}
-#' 
-#' # PARM Dataframe:
-#' parms <- data.frame(idx=as.character(), 
-#'                     Pm.mean = as.numeric(), 
-#'                     Pm.se = as.numeric(),
-#'                     Pm.Bulk_ESS= as.numeric() ,
-#'                     Pm.Tail_ESS= as.numeric() ,
-#'                     Pm.Rhat= as.numeric(),
-#'                     
-#'                     alpha.mean = as.numeric(),
-#'                     alpha.se = as.numeric(),
-#'                     alpha.Bulk_ESS= as.numeric() ,
-#'                     alpha.Tail_ESS= as.numeric() ,
-#'                     alpha.Rhat= as.numeric(),
-#'                     
-#'                     Icomp.mean = as.numeric(),
-#'                     Icomp.se= as.numeric(),
-#'                     Icomp.Bulk_ESS= as.numeric() ,
-#'                     Icomp.Tail_ESS= as.numeric() ,
-#'                     Icomp.Rhat= as.numeric(),
-#'                     samples= as.numeric())
-#' 
-#' 
-#' 
-#' for ( i in unique(df$idx)){
-#'   print(i)
-#'    
-#'   # Subset the file:
-#'   df.sub <- df %>% filter(idx == i, PAR > 0) %>% na.omit
-#' 
-#'   # get priors:
-#'    #priors <- get_prior(bf(equation, a1+ax+r ~ 1, nl=TRUE),data = df %>% filter(PAR > 0), family = poisson())
-#'   
-#'   try(model.brms <- brm( bf( equation, Pm+alpha+Icomp ~ 1, nl=TRUE), prior = priors.lrc, 
-#'                          data = df.sub, iter = iterations, cores =3, chains = 1, backend = "cmdstanr"), silent= F)
-#'   # Here it should be df.sub
-#'    print(model.brms)
-#'   
-#'   try(model.brms.df <- summary(model.brms)$fixed , silent = T)
-#'   try(model.brms.df.Pm <- model.brms.df %>% filter( row.names(model.brms.df) == 'Pm_Intercept'), silent = F)
-#'   try(model.brms.df.alpha <- model.brms.df %>% filter( row.names(model.brms.df) == 'alpha_Intercept'), silent = F)
-#'   try(model.brms.df.Icomp <- model.brms.df %>% filter( row.names(model.brms.df) == 'Icomp_Intercept'), silent = F)
-#'   
-#'   samples <- data.frame %>% filter(YearMon == i)%>% select(nee)  %>% na.omit %>% nrow
-#'   
-#'   baseline <- as.Date(paste(i, '-01', sep="")) %>% lubridate::days_in_month() *48 %>% as.numeric
-#'   
-#'   
-#'   try(results <- data.frame( idx = i, 
-#'                          Pm.mean = model.brms.df.Pm$Estimate ,
-#'                          Pm.se = model.brms.df.Pm$Est.Error ,
-#'                          Pm.Bulk_ESS = model.brms.df.Pm$Bulk_ESS , 
-#'                          Pm.Tail_ESS = model.brms.df.Pm$Tail_ESS ,
-#'                          Pm.Rhat = model.brms.df.Pm$Rhat ,
-#'                          
-#'                          alpha.mean = model.brms.df.alpha$Estimate ,
-#'                          alpha.se = model.brms.df.alpha$Est.Error ,
-#'                          alpha.Bulk_ESS = model.brms.df.alpha$Bulk_ESS , 
-#'                          alpha.Tail_ESS = model.brms.df.alpha$Tail_ESS ,
-#'                          alpha.Rhat = model.brms.df.alpha$Rhat ,
-#'                          
-#'                          Icomp.mean = model.brms.df.Icomp$Estimate ,
-#'                          Icomp.se = model.brms.df.Icomp$Est.Error ,
-#'                          Icomp.Bulk_ESS = model.brms.df.Icomp$Bulk_ESS , 
-#'                          Icomp.Tail_ESS = model.brms.df.Icomp$Tail_ESS ,
-#'                          Icomp.Rhat = model.brms.df.Icomp$Rhat,
-#'                          samples= samples/baseline *100 ), silent = T)
-#'   
-#'  print(results)
-#'  
-#'   try(parms <- parms %>% rbind(results), silent = T)
-#' }
-#'  
-#' return(parms ) 
-#' }
-
-# EOF
-
