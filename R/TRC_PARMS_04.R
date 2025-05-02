d379545a72f1f9e04abd2f1a3b38ae02d2350591
Angel Chen <angelchen7777@yahoo.com>
2025-05-02 15:17:25 -0400

    docs: created function documentation for TRC_PARMS_04.R

--- R/TRC_PARMS_04.R
+++ R/TRC_PARMS_04.R
@@ -1,157 +1,146 @@
-#' fit a Temperature Response Curves (TRC) by an index to get a parameter file
+#' @title Fit a Temperature Response Curve (TRC) by an index to get a parameter file
 #'
-#' `TRC_PARMS` returns a dataframe of parameter values by the index used to fit them.
-#' `TRC_PARMS` requires a few arguments @data.frame,  @iterations, @priors, @idx.colname, @NEE.colname, @PAR.colname, and @TA.colname.
-#' `dataframe` is a dataframe that containes NEE, idx, TA, and PAR.
-#' `iterations` is the number of iterations to run brms. 
-#' `priors.trc ` is the priors for the brms to use.
-#' `idx.colname` is the column containing the index.
-#' `NEE.colname` is the name of the column containing NEE.
-#' `PAR.colname` is the name of the column containing PAR.
-#' `TA.colname` is the name of the column containing air temperature.
+#' @description
+#' This function uses the equation:
+#' \deqn{\text{NEE} \sim R_{\text{ref}} \cdot Q_{10} \cdot \exp \left( \frac{T - T_{\text{ref}}}{10} \right)}
+#'
+#' The equation requires air temperature (TA) in degrees Celsius, photosynthetically active radiation (PAR) in 𝜇mol m-2 s-1, and net ecosystem exchange (NEE) in 𝜇mol m-2 s-1.
+#'
+#' @details
+#' Model parameters are fit using the R package `brms`.
+#'
+#' Rhat (Potential Scale Reduction Factor):
+#' Indicates how well the different Markov chains in your analysis have converged to the same posterior distribution.
+#' Ideally, Rhat should be close to 1 for all parameters.
+#' A high Rhat value suggests potential convergence issues and the need to run the chains longer.
+#'
+#' Bulk ESS (Effective Sample Size - Bulk):
+#' Estimates the effective number of independent samples from the central part of the posterior distribution.
+#'
+#' Tail ESS (Effective Sample Size - Tail):
+#' Estimates the effective number of independent samples from the tails of the posterior distribution.
+#' Important for assessing the reliability of quantile estimates (e.g., 95% confidence intervals).
+#'
+#' Key points to remember:
+#' Aim for Rhat close to 1 and high values for both Bulk ESS and Tail ESS.
+#'
+#' @param data.frame (dataframe) A dataframe that contains net ecosystem exchange (NEE), an index, air temperature (TA), and photosynthetically active radiation (PAR).
+#' @param iterations (numeric) The number of iterations to run `brms::brm()`.
+#' @param priors.trc (brmsprior dataframe) The priors for `brms::brm()` to use.
+#' Default priors are as follows:
+#' ```
+#' brms::prior(normal(2.0, 0.3), nlpar = "Q10", lb = 1.0, ub = 3.5) +
+#' brms::prior(normal(0.5, 0.3), nlpar = "Rref", lb = 0.01, ub = 1)
+#' ```
+#' @param idx.colname (character) The name of the column containing the index.
+#' @param NEE.colname (character) The name of the column containing NEE.
+#' @param TA.colname (character) The name of the column containing air temperature.
+#' @param PAR.colname (character) The name of the column containing PAR.
+#'
+#' @returns (dataframe) Dataframe of parameter values by the index used to fit them.
+#' @export
+#'
+#' @examples
+#' # Write examples here
+#'
+TRC_PARMS_04 <- function(data.frame = NULL,
+                         iterations= NULL,
+                         priors.trc = brms::prior(normal(2.0, 0.3), nlpar = "Q10", lb = 1.0, ub = 3.5) +
+                           brms::prior(normal(0.5, 0.3), nlpar = "Rref", lb = 0.01, ub = 1),
+                         idx.colname = NULL,
+                         NEE.colname = NULL,
+                         TA.colname = NULL,
+                         PAR.colname = NULL){
 
-library(brms) 
-library(cmdstanr)
-library(ggplot2)
-library(beepr)
-library(tidybayes)
-library(tidyverse)
-library(ggpubr)
-
-message("Using the TRC_PARMS function requires the following libraries: brms, 
-        cmdstanr, ggplot2, beepr, tidybayes, tidyverse, and ggpubr.")
-
-message("This function uses the equation:
-        
-        Reco ~ Rref * Q10 exp((TA-Tref)/10 ) 
-        
-        TA must be in C")
-
-
-
-# Example of priors: 
-priors.trc <- prior(normal( 2.0 ,  0.3), nlpar = "Q10", lb=1.0, ub= 3.5)+
-  prior(normal( 0.5 ,  0.3), nlpar = "Rref", lb=0.01, ub= 1)
-
-message("To see the default priors run: 'priors.trc' ")
-
-TRC_PARMS_04 <- function( data.frame, iterations, priors.trc, idx.colname, Reco.colname, TA.colname, PAR.colname){
-  
-  data.frame$nee <- data.frame[,Reco.colname]
+  data.frame$nee <- data.frame[,NEE.colname]
   data.frame$idx <- data.frame[,idx.colname]
   data.frame$TA <- data.frame[,TA.colname]
   data.frame$PAR <- data.frame[,PAR.colname]
-  
-  #data.frame$TA <-data.frame$TA+273.15 #celsius to kelvin
-  
-  df <- data.frame %>% select(idx, Reco, TA, PAR)
-  
-  if( c("idx") %in% names(df) ) {
-    print("GREAT JOB! your dataframe contains idx")
-  } else{ print("The dataframe must include: idx,  Reco, TA")}
-  
-  if( c("Reco") %in% names(df) ) {
-    print("YIPEE! your dataframe contains Reco")
-  }  else{ print("The dataframe must include: idx,  Reco, TA")}
-  
-  if( c("PAR") %in% names(df) ) {
-    print("Hooray! your dataframe contains PAR")
-  } else{ print("The dataframe must include: idx,  nee, and PAR")}
 
-  if( c("TA") %in% names(df) ) {
-    print("Hooray! your dataframe contains TA")
-  } else{ print("The dataframe must include: idx,  Reco, TA")}
-  try(equation <- Reco ~ Rref * Q10 * exp((TA-10)/10) , silent =T)
-  
+  #data.frame$TA <-data.frame$TA+273.15 #celsius to kelvin
+
+  df <- data.frame %>% dplyr::select(idx, nee, TA, PAR)
+
+  if(c("idx") %in% base::names(df)) {
+    base::print("GREAT JOB! your dataframe contains idx")
+  } else {base::print("The dataframe must include: idx, nee, TA, and PAR")}
+
+  if(c("nee") %in% base::names(df)) {
+    base::print("YIPEE! your dataframe contains nee")
+  } else {base::print("The dataframe must include: idx, nee, TA, and PAR")}
+
+  if(c("PAR") %in% base::names(df)) {
+    base::print("Hooray! your dataframe contains PAR")
+  } else {base::print("The dataframe must include: idx, nee, TA, and PAR")}
+
+  if(c("TA") %in% base::names(df)) {
+    base::print("Hooray! your dataframe contains TA")
+  } else {base::print("The dataframe must include: idx, nee, TA, and PAR")}
+  base::try(equation <- nee ~ Rref * Q10 * exp((TA-10)/10) , silent = T)
+
   # PARM Dataframe:
-  try(parms <- data.frame(idx=as.character(), 
-                          Q10.mean = as.numeric(), 
-                          Q10.se = as.numeric(),
-                          Q10.Bulk_ESS= as.numeric() ,
-                          Q10.Tail_ESS= as.numeric() ,
-                          Q10.Rhat= as.numeric(),
-                          
-                          Rref.mean = as.numeric(),
-                          Rref.se = as.numeric(),
-                          Rref.Bulk_ESS= as.numeric() ,
-                          Rref.Tail_ESS= as.numeric() ,
-                          Rref.Rhat= as.numeric(),
-                          samples= as.numeric()), silent = T)
-  
-  message(" Your dataframe looks good and you are now ready to start fitting models")
-  
-  for ( i in unique(df$idx)){
-    print(i)
-    
+  base::try(parms <- base::data.frame(idx = base::as.character(),
+                                      Q10.mean = base::as.numeric(),
+                                      Q10.se = base::as.numeric(),
+                                      Q10.Bulk_ESS = base::as.numeric(),
+                                      Q10.Tail_ESS = base::as.numeric(),
+                                      Q10.Rhat = base::as.numeric(),
+
+                                      Rref.mean = base::as.numeric(),
+                                      Rref.se = base::as.numeric(),
+                                      Rref.Bulk_ESS = base::as.numeric(),
+                                      Rref.Tail_ESS = base::as.numeric(),
+                                      Rref.Rhat = base::as.numeric(),
+                                      samples = base::as.numeric()), silent = T)
+
+  base::message(" Your dataframe looks good and you are now ready to start fitting models")
+
+  for (i in base::unique(df$idx)){
+    base::print(i)
+
     # Subset the file:
-    df.sub <- try(df %>% filter(idx == i, PAR < 10), silent = TRUE)
+    df.sub <- base::try(df %>% dplyr::filter(idx == i, PAR < 10), silent = TRUE)
 
     # get priors:
-    print(df.sub)
+    base::print(df.sub)
     # priors <- get_prior(bf(equation, a1+ax+r ~ 1, nl=TRUE),
     #                    data = df %>% filter(PAR > 0),
     #                    family = poisson())
-    try(model.brms <-  brm( bf(Reco ~ Rref * Q10 * exp((TA-10)/10),Q10+Rref ~ 1, nl=TRUE),
-                            prior = priors.trc , data = df.sub, 
-                            backend = "cmdstanr", iter = iterations, cores =4, seed=101), silent= F)
-    
-    print(model.brms)
-    
-    try(model.brms.df <- summary(model.brms)$fixed , silent= F)
-    
-    try(model.brms.df.a <- model.brms.df %>% filter( row.names(model.brms.df) == 'Q10_Intercept'), silent= F)
-    try(model.brms.df.b <- model.brms.df %>% filter( row.names(model.brms.df) == 'Rref_Intercept'), silent= F)
-    
-    try(samples <- data.frame %>% filter(YearMon == i)%>% select(nee)  %>% na.omit %>% nrow , silent= F)
-    try(baseline <- as.Date(paste(i, '-01', sep="")) %>% lubridate::days_in_month() *48 %>% as.numeric, silent= F)
-    
-    
-    try(results <- data.frame( idx = i, 
-                               Q10.mean = model.brms.df.Q10$Estimate ,
-                               Q10.se = model.brms.df.Q10$Est.Error ,
-                               Q10.Bulk_ESS = model.brms.df.Q10$Bulk_ESS , 
-                               Q10.Tail_ESS = model.brms.df.Q10$Tail_ESS ,
-                               Q10.Rhat = model.brms.df.Q10$Rhat ,
-                               
-                               Rref.mean = model.brms.df.Rref$Estimate ,
-                               Rref.se = model.brms.df.Rref$Est.Error ,
-                               Rref.Bulk_ESS = model.brms.df.Rref$Bulk_ESS , 
-                               Rref.Tail_ESS = model.brms.df.Rref$Tail_ESS ,
-                               Rref.Rhat = model.brms.df.Rref$Rhat,
-                               samples= samples/baseline *100 ), silent= T)
-    
-    message( 'YOUR DID IT!')
-    print( results) 
-    try( parms <- parms %>% rbind(results), silent= T)
-    
+    base::try(model.brms <- brms::brm(brms::bf(nee ~ Rref * Q10 * exp((TA-10)/10), Q10+Rref ~ 1, nl = TRUE),
+                                      prior = priors.trc , data = df.sub,
+                                      backend = "cmdstanr", iter = iterations, cores = 4, seed = 101), silent = F)
+
+    base::print(model.brms)
+
+    base::try(model.brms.df <- summary(model.brms)$fixed, silent = F)
+
+    base::try(model.brms.df.a <- model.brms.df %>% dplyr::filter(base::row.names(model.brms.df) == 'Q10_Intercept'), silent = F)
+    base::try(model.brms.df.b <- model.brms.df %>% dplyr::filter(base::row.names(model.brms.df) == 'Rref_Intercept'), silent = F)
+
+    base::try(samples <- data.frame %>% dplyr::filter(YearMon == i) %>% dplyr::select(nee) %>% stats::na.omit() %>% base::nrow(), silent = F)
+    base::try(baseline <- base::as.Date(base::paste(i, '-01', sep = "")) %>% lubridate::days_in_month()*48 %>% base::as.numeric(), silent = F)
+
+
+    base::try(results <- base::data.frame(idx = i,
+                                          Q10.mean = model.brms.df.Q10$Estimate,
+                                          Q10.se = model.brms.df.Q10$Est.Error,
+                                          Q10.Bulk_ESS = model.brms.df.Q10$Bulk_ESS,
+                                          Q10.Tail_ESS = model.brms.df.Q10$Tail_ESS,
+                                          Q10.Rhat = model.brms.df.Q10$Rhat,
+
+                                          Rref.mean = model.brms.df.Rref$Estimate,
+                                          Rref.se = model.brms.df.Rref$Est.Error,
+                                          Rref.Bulk_ESS = model.brms.df.Rref$Bulk_ESS,
+                                          Rref.Tail_ESS = model.brms.df.Rref$Tail_ESS,
+                                          Rref.Rhat = model.brms.df.Rref$Rhat,
+                                          samples = samples/baseline*100), silent = T)
+
+    base::message('YOU DID IT!')
+    base::print(results)
+    base::try(parms <- parms %>% base::rbind(results), silent = T)
+
   }
-  
-  return(parms ) 
+
+  return(parms)
 }
-
-message("Model parameters Are fit using the R package brms.
-        
-        Rhat (Potential Scale Reduction Factor):
-        
-        Indicates how well the different Markov chains in your analysis 
-        have converged to the same posterior distribution. 
-        
-        Ideally, Rhat should be close to 1 for all parameters.
-        
-        A high Rhat value suggests potential convergence issues and 
-        the need to run the chains longer. 
-        
-        Bulk ESS (Effective Sample Size - Bulk):
-        
-        Estimates the effective number of independent samples from the 
-        central part of the posterior distribution. 
-        
-        Tail ESS (Effective Sample Size - Tail):
-        Estimates the effective number of independent samples from the tails of the posterior distribution. 
-        
-        Important for assessing the reliability of quantile estimates (e.g., 95% confidence intervals). 
-        
-        Key points to remember:
-        Aim for Rhat close to 1 and high values for both Bulk ESS and Tail ESS." )
-
 #EOF
